6e45253c9abb58eefb3c6b5d2fad788fd3560523
Andrea Bianchi <andrea@kaist.ac.kr>
2020-07-20 14:17:39 +0900

    Working on new firmware

---
+++ Software/Arduino/BlinkBoardFirmware/include/constants.hpp
@@ -0,0 +1,23 @@
+#ifndef __CONSTANTS__H__
+#define __CONSTANTS__H__
+
+#define LATCH1 8
+#define LATCH2 9
+#define LATCH3 10
+#define DATA 11
+#define CLK 12
+#define RESET 13
+
+#define INPUT_A0 A0
+#define INPUT_A1 A1
+#define INPUT_A2 A2
+#define OUTPUT_D0 5
+#define OUTPUT_D1 6
+// D2 is attached to DAC
+#define PATTERN2 3
+
+#define PATTERN2_PERIOD_MS 100
+#define BUFFER_SIZE 128
+
+#endif
+
---
+++ Software/Arduino/BlinkBoardFirmware/src/main.cpp
@@ -0,0 +1,146 @@
+#include <Arduino.h>
+
+// Libs
+#include "MsTimer2.h"
+#include "ArduinoJson.h"
+
+// Constants
+#include "constants.hpp"
+
+// Types
+enum LED_State { ON, OFF, PTRN1, PTRN2 };
+
+// Forward declarations
+void initialize();
+void blink();
+void parse(const String &buffer);
+void ack(const String &msg);
+void sendCommand (uint8_t ledNumber, LED_State state);
+void reset();
+
+// Globals
+String buffer = "";
+
+
+
+// Main
+
+void setup()
+{
+  initialize();
+  reset();
+
+  MsTimer2::set(PATTERN2_PERIOD_MS, blink);
+  MsTimer2::start();
+}
+
+void loop()
+{
+  while (Serial.available())
+  {
+    char inChar = (char)Serial.read();
+    if (inChar == '\n')
+    {
+      parse(buffer);
+      buffer = "";
+    }
+    else
+    {
+      buffer += inChar;
+    }
+  }
+}
+
+
+// Helpers
+
+void initialize()
+{
+  Serial.begin(115200);
+  ack(F("ready"));
+
+  pinMode(LATCH1, OUTPUT);
+  pinMode(LATCH2, OUTPUT);
+  pinMode(LATCH3, OUTPUT);
+  pinMode(DATA, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  pinMode(RESET, OUTPUT);
+
+  pinMode(OUTPUT_D0, OUTPUT);
+  pinMode(OUTPUT_D1, OUTPUT);
+  pinMode(PATTERN2, OUTPUT);
+}
+
+
+void blink()
+{
+  static boolean output = HIGH;
+
+  digitalWrite(PATTERN2, output);
+  output = !output;
+}
+
+void reset()
+{
+
+}
+
+
+void parse(const String &buffer)
+{
+  StaticJsonDocument<BUFFER_SIZE> json;
+  DeserializationError error = deserializeJson(json, buffer);
+
+  // Test if parsing succeeds.
+  if (error)
+  {
+    ack(F("json parse fail"));
+    return;
+  }
+
+  // Parse JSON
+  if (json["cmd"] == F("reset"))
+  {
+    // do something
+    ack(F("ok"));
+  }
+  else
+  {
+    ack(F("invalid command"));
+  }
+}
+
+void ack(const String &msg)
+{
+  String result = "{\"ack\": \"" + msg + "\"}";
+  Serial.println(result);
+}
+
+
+void sendCommand (uint8_t ledNumber, LED_State state)
+{
+  byte op = 0;
+  switch (state)
+  {
+    case OFF: op= 0; break;   // 00
+    case ON: op= 1; break;    // 01
+    case PTRN1: op= 2; break; // 10
+    case PTRN2: op= 3; break; // 11
+  }
+
+  digitalWrite(LATCH1, LOW);
+  shiftOut(DATA, CLK, MSBFIRST, ledNumber | (op << 5 ));
+  digitalWrite(LATCH1, HIGH);
+}
+
+
+/*
+  sendCommand (0, BLINK);
+  sendCommand (1, BLINK);
+  sendCommand (2, BLINK);
+  sendCommand (3, OFF);
+  sendCommand (4, OFF);
+  sendCommand (5, ON);
+  sendCommand (6, ON);
+  sendCommand (7, BLINK2);
+*/
